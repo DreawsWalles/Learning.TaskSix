05ab46ee35b69f97ec6ed18617394915451c27ff
Андрей Спицын <a.spitsin@qpdev.ru>
2021-12-14 21:36:08 +0300

    test

--- taskTwo/Program.cs
+++ taskTwo/Program.cs
@@ -14,25 +14,25 @@ namespace taskTwo
     {
         public static void Main()
         {
-            IView view = new ViewConsole();
-            Logger logger = new Logger();
-            try
-            {
-                ChatBot bot = new ChatBot(new History(view), new UnitOfWork(new XMLAphorismsRepository(), new JSONMyNameRepository(), new XMLJokeRepository(), new XMLByeRepository(), new FakeHelpRepository(), new FakeDownloadWebSiteRepository()), view);
+            //IView view = new ViewConsole();
+            //Logger logger = new Logger();
+            //try
+            //{
+            //    ChatBot bot = new ChatBot(new History(view), new UnitOfWork(new XMLAphorismsRepository(), new JSONMyNameRepository(), new XMLJokeRepository(), new XMLByeRepository(), new FakeHelpRepository(), new FakeDownloadWebSiteRepository()), view);
 
-                string task = "";
-                while (task.ToLower() != "пока" && task.ToLower() != "до свидания")
-                {
-                    Console.Write("You->");
-                    task = Console.ReadLine();
-                    bot.Ask(task);
-                }
-            }
-            catch(Exception e)
-            {
-                logger.Log(e.Message);
-                view.ViewError(e.Message);
-            }
+            //    string task = "";
+            //    while (task.ToLower() != "пока" && task.ToLower() != "до свидания")
+            //    {
+            //        Console.Write("You->");
+            //        task = Console.ReadLine();
+            //        bot.Ask(task);
+            //    }
+            //}
+            //catch(Exception e)
+            //{
+            //    logger.Log(e.Message);
+            //    view.ViewError(e.Message);
+            //}
         }
     }
 }
